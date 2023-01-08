9a04c292ecec6233be7de67e1923313454fda478
yash <yash.khandelwal@aurigait.com>
2023-01-08 16:55:45 +0530

    Email & Password Validations Added

--- src/pages/Login..js
+++ src/pages/Login..js
@@ -1,6 +1,6 @@
 import { Button, Card, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
 
@@ -8,23 +8,69 @@ const Login = () => {
     const location = useLocation();
 
     const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+
     const [password, setPassword] = useState('');
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
 
     useEffect(() => {
         console.log("Got From To Signup")
-        setEmail(location.state.email)
+        if (location != null && location.state != null && location.state.email != null) setEmail(location.state.email)
     }, [])
 
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+    function validatePassword(password) {
+        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return re.test(password);
+    }
+
     function continueToSignup() {
+
+        let isEmailValid = false;
+        let isPasswordValid = false;
+
+        if (!validateEmail(email)) {
+            setIsEmailValid(false);
+            isEmailValid = false;
+        } else {
+            setIsEmailValid(true);
+            isEmailValid = true;
+        }
+
+        if (!validatePassword(password)) {
+            setIsPasswordValid(false);
+            isPasswordValid = false;
+        } else {
+            setIsPasswordValid(true);
+            isPasswordValid = true;
+        }
+
+        if (!isEmailValid || !isPasswordValid) return;
+
         console.log("Transfer To Signup");
+
         navigation("/signup", { state: { "email": email, "password": password } });
     }
+
     return (<>
         <Card variant="outlined">
             <div style={{ padding: '45px' }}>
                 <div style={{ fontWeight: '700', fontSize: '24px', lineHeight: '33px', color: '#343435', paddingBottom: '145px' }}>Login</div>
-                <TextField fullWidth id="email" label="Email ID" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }} value={email} onChange={event => setEmail(event.target.value)} />
-                <TextField fullWidth id="password" label="Enter Password" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '7px' }} value={password} onChange={event => setPassword(event.target.value)} />
+
+                <TextField fullWidth id="email" label="Email ID" variant="outlined"
+                    style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }}
+                    value={email} onChange={event => setEmail(event.target.value)}
+                    error={!isEmailValid} helperText={!isEmailValid && "Incorrect Email"} type="email" />
+
+                <TextField fullWidth id="password" label="Enter Password" variant="outlined"
+                    style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '7px' }}
+                    value={password} onChange={event => setPassword(event.target.value)}
+                    error={!isPasswordValid} helperText={!isPasswordValid && "Incorrect Password"} type="password" />
+
                 <div style={{ textAlign: 'right' }}>
                     <Button variant="text" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', textTransform: 'none', textAlign: 'right' }}>Forget Password?</Button>
                 </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,17 +5,65 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Signup = () => {
 
-    const [email, setEmail] = useState('');
-
     const navigation = useNavigate();
     const location = useLocation();
 
+    const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+
+    const [password, setPassword] = useState('');
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isPasswordsSame, setIsPasswordsSame] = useState(true);
+
     useEffect(() => {
         console.log("Got From To Login")
-        setEmail(location.state.email)
+        if (location != null && location.state != null && location.state.email != null) setEmail(location.state.email)
     }, [])
 
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+    function validatePassword(password) {
+        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return re.test(password);
+    }
+
     function continueToLogin() {
+
+        let isEmailValid = false;
+        let isPasswordValid = false;
+        let isPasswordsSame = false;
+
+        if (!validateEmail(email)) {
+            setIsEmailValid(false);
+            isEmailValid = false;
+        } else {
+            setIsEmailValid(true);
+            isEmailValid = true;
+        }
+
+        if (!validatePassword(password)) {
+            setIsPasswordValid(false);
+            isPasswordValid = false;
+        } else {
+            setIsPasswordValid(true);
+            isPasswordValid = true;
+        }
+
+        if (password !== confirmPassword) {
+            setIsPasswordsSame(false);
+            isPasswordsSame = false;
+        } else {
+            setIsPasswordsSame(true);
+            isPasswordsSame = true;
+        }
+
+        if (!isEmailValid || !isPasswordValid || !isPasswordsSame) return;
+
         console.log("Transfer To Login");
         navigation("/login", { state: { "email": email } });
     }
@@ -25,7 +73,7 @@ const Signup = () => {
             <div style={{ padding: '45px' }}>
                 <div style={{ fontWeight: '700', fontSize: '24px', lineHeight: '33px', color: '#343435', paddingBottom: '30px' }}>Sign up</div>
                 <TextField fullWidth id="outlined-basic" label="Name" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }} />
-                <TextField fullWidth id="outlined-basic" label="Email ID" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '30px' }} value={email} />
+                <TextField fullWidth id="email" label="Email ID" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }} value={email} onChange={event => setEmail(event.target.value)} error={!isEmailValid} helperText={!isEmailValid && "Incorrect Email"} type="email" />
                 <p style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', textAlign: 'left' }}>Choose your role</p>
 
                 <Box sx={{ display: 'flex' }} style={{ marginBottom: '32px' }}>
@@ -41,8 +89,17 @@ const Signup = () => {
                         label="Tournament Admin" />
 
                 </Box>
-                <TextField fullWidth id="outlined-basic" label="Enter Password" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }} />
-                <TextField fullWidth id="outlined-basic" label="Confirm Password" variant="outlined" style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435' }} />
+
+                <TextField fullWidth label="Enter Password" variant="outlined"
+                    style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435', marginBottom: '34px' }}
+                    type="password" onChange={event => setPassword(event.target.value)}
+                    error={!isPasswordValid} helperText={!isPasswordValid && "Suggest a Strong Password"} />
+
+                <TextField fullWidth label="Confirm Password" variant="outlined"
+                    style={{ fontWeight: '400', fontSize: '14px', lineHeight: '19px', color: '#343435' }}
+                    type="password" onChange={event => setConfirmPassword(event.target.value)}
+                    error={!isPasswordsSame} helperText={!isPasswordsSame && "Password Mis-Match"} />
+
             </div>
             <div style={{ paddingBottom: '22px' }}>
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
